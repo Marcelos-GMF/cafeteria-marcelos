bfb9b061635211d872aa60f78e924bbd4027b28d
Marcelo Silva <marcelomaneiro@gmail.com>
2024-07-23 18:30:52 -0300

    Explicação Rota:
    Nova forma de chamar rota em novas versões do angular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,10 +4,12 @@ export const APP_ROUTES: Routes = [
   {
     path: '', pathMatch: 'full', redirectTo: 'products'
   },
+  // Arquivos de rotas para varios componentes lazy-loaded
   {
     path: 'products',
     loadChildren: () => import('./products/products.routes').then(r => r.PRODUCT_ROUTES)
   },
+  // Rota para o carrinho de um componente lazy-loaded
   {
     path: 'cart',
     loadComponent: () => import('./cart/cart/cart.component').then(c => c.CartComponent)
